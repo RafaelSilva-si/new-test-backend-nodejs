66bf29662bc0e3392ed7b655442be98a029aa63a
Rafael Mariano da Silva <seu_email@exemplo.com>
2024-10-12 19:26:01 -0300

    feat: should returns error if category not found

--- src/product/services/associate-category-product.service.ts
+++ src/product/services/associate-category-product.service.ts
@@ -1,9 +1,13 @@
 import { MissingParamError } from '../../errors/throw-missing-param.error';
 import { NotFoundError } from '../../errors/throw-not-found.error';
+import { CategoryRepository } from '../../repositories/category.repository';
 import { ProductRepository } from '../../repositories/product.repository';
 
 export class AssociateCategoryProductService {
-  constructor(readonly productRepository: ProductRepository) {}
+  constructor(
+    readonly productRepository: ProductRepository,
+    readonly categoryRepository: CategoryRepository
+  ) {}
 
   async execute(associateCategoryProductDto: any) {
     const requiredFields = ['productId', 'categoryId', 'ownerId'];
@@ -20,6 +24,15 @@ export class AssociateCategoryProductService {
 
     if (!product) {
       throw new NotFoundError('Product not found');
+    }
+
+    const category = await this.categoryRepository.findById(
+      associateCategoryProductDto.categoryId,
+      associateCategoryProductDto.ownerId
+    );
+
+    if (!category) {
+      throw new NotFoundError('Category not found');
     }
   }
 }
