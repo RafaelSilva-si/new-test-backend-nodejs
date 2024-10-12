02beed5a6e84b1d113ada37df321ea7db7f79a77
Rafael Mariano da Silva <seu_email@exemplo.com>
2024-10-12 18:09:48 -0300

    feat: should retuurns error if product not found

---
+++ src/product/services/associate-category-product.service.ts
@@ -0,0 +1,24 @@
+import { MissingParamError } from '../../errors/throw-missing-param.error';
+import { NotFoundError } from '../../errors/throw-not-found.error';
+import { ProductRepository } from '../../repositories/product.repository';
+
+export class AssociateCategoryProductService {
+  constructor(readonly productRepository: ProductRepository) {}
+
+  async execute(associateCategoryProductDto: any) {
+    const requiredFields = ['productId', 'categoryId', 'ownerId'];
+    for (const field of requiredFields) {
+      if (associateCategoryProductDto[field as keyof any] === undefined) {
+        throw new MissingParamError(field);
+      }
+    }
+
+    const product = await this.productRepository.findById(
+      associateCategoryProductDto.productId
+    );
+
+    if (!product) {
+      throw new NotFoundError('Product not found');
+    }
+  }
+}
