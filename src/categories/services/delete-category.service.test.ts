9c3360889ceef71a38f57091cdce459c086e9b04
Rafael Mariano da Silva <seu_email@exemplo.com>
2024-10-13 12:05:10 -0300

    feat: should returns error if category doesnt exists

--- src/categories/services/delete-category.service.test.ts
+++ src/categories/services/delete-category.service.test.ts
@@ -8,7 +8,7 @@ describe('Delete Category', () => {
   beforeEach(() => {
     categoryRepository = new CategoryRepository();
 
-    deleteCategoryService = new DeleteCategoryService(new CategoryRepository());
+    deleteCategoryService = new DeleteCategoryService(categoryRepository);
   });
 
   it('Should returns error if required fields are missing', async () => {
@@ -20,5 +20,18 @@ describe('Delete Category', () => {
     await expect(
       deleteCategoryService.execute(requestData as any)
     ).rejects.toThrow('Missing param: id');
+  });
+
+  it("should returns error if category doesn't exists", async () => {
+    const requestData = {
+      id: '1',
+      ownerId: 'teste',
+    };
+
+    jest.spyOn(categoryRepository, 'findById').mockResolvedValueOnce(null);
+
+    await expect(
+      deleteCategoryService.execute(requestData as any)
+    ).rejects.toThrow('Category not found');
   });
 });
