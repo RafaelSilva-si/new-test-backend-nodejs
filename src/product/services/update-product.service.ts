ae8ec70b5e4547f33adff3786aeaf90fa712be16
Rafael Mariano da Silva <seu_email@exemplo.com>
2024-10-13 11:12:15 -0300

    feat: should returns error if product not found

--- src/product/services/update-product.service.ts
+++ src/product/services/update-product.service.ts
@@ -1,13 +1,26 @@
 import { MissingParamError } from '../../errors/throw-missing-param.error';
+import { NotFoundError } from '../../errors/throw-not-found.error';
+import { ProductRepository } from '../../repositories/product.repository';
 import { UpdateProductDto } from '../types/update-product-dto';
 
 export class UpdateProductService {
-  async execute(id: string, updateProductDto: UpdateProductDto) {
-    const requiredFields = ['ownerId'];
+  constructor(private readonly productRepository: ProductRepository) {}
+
+  async execute(updateProductDto: UpdateProductDto) {
+    const requiredFields = ['ownerId', 'id'];
     for (const field of requiredFields) {
       if (updateProductDto[field as keyof UpdateProductDto] === undefined) {
         throw new MissingParamError(field);
       }
+    }
+
+    const product = await this.productRepository.findById(
+      updateProductDto.id,
+      updateProductDto.ownerId
+    );
+
+    if (!product) {
+      throw new NotFoundError('Product not found');
     }
     return updateProductDto;
   }
