2e9482344e75a6ed55962c5de5e0b308539c4e66
Rafael Mariano da Silva <seu_email@exemplo.com>
2024-10-13 11:17:02 -0300

    feat: create update product repository function

--- src/repositories/product.repository.ts
+++ src/repositories/product.repository.ts
@@ -1,6 +1,7 @@
 import { AssociateCategoryProductDto } from '../product/types/associate-category-product.dto';
 import { CreateProductDto } from '../product/types/create-product-dto';
 import { Product } from '../product/types/product.type';
+import { UpdateProductDto } from '../product/types/update-product-dto';
 
 export class ProductRepository {
   async create(createProductDto: CreateProductDto): Promise<Product> {
@@ -31,6 +32,17 @@ export class ProductRepository {
       price: 1,
       category: associateCategoryProductDto.categoryId,
       ownerId: '1',
+    };
+  }
+
+  async update(updateProductDto: UpdateProductDto): Promise<Product> {
+    return {
+      id: updateProductDto.id,
+      title: updateProductDto.title,
+      description: updateProductDto.description,
+      price: updateProductDto.price,
+      category: updateProductDto.category,
+      ownerId: updateProductDto.ownerId,
     };
   }
 }
