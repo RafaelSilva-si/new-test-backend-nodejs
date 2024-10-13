0a04caa4ea429ff2f6f29193617809f479181f64
Rafael Mariano da Silva <seu_email@exemplo.com>
2024-10-13 11:28:33 -0300

    feat: should returns error if category doesnt exists

--- src/categories/services/update-category.service.ts
+++ src/categories/services/update-category.service.ts
@@ -1,9 +1,11 @@
 import { MissingParamError } from '../../errors/throw-missing-param.error';
+import { NotFoundError } from '../../errors/throw-not-found.error';
+import { CategoryRepository } from '../../repositories/category.repository';
 import { Category } from '../types/category.type';
 import { UpdateCategoryDto } from '../types/update-category.dto';
 
 export class UpdateCategoryService {
-  constructor() {}
+  constructor(private readonly categoryRepository: CategoryRepository) {}
 
   async execute(updateCategoryDto: UpdateCategoryDto): Promise<Category> {
     const requiredFields = ['id', 'ownerId'];
@@ -12,6 +14,15 @@ export class UpdateCategoryService {
       if (updateCategoryDto[field as keyof UpdateCategoryDto] === undefined) {
         throw new MissingParamError(field);
       }
+    }
+
+    const category = await this.categoryRepository.findById(
+      updateCategoryDto.id,
+      updateCategoryDto.ownerId
+    );
+
+    if (!category) {
+      throw new NotFoundError('Category not found');
     }
 
     return updateCategoryDto;
