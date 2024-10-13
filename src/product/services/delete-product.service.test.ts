a916ea750ffdfe45c5eafce2c8195a25a13ae85d
Rafael Mariano da Silva <seu_email@exemplo.com>
2024-10-13 11:44:25 -0300

    test: should returns error if produuct doesnt exists

--- src/product/services/delete-product.service.test.ts
+++ src/product/services/delete-product.service.test.ts
@@ -1,10 +1,14 @@
+import { ProductRepository } from '../../repositories/product.repository';
 import { DeleteProductService } from './delete-product.service';
 
 describe('Delete Product', () => {
+  let productRepository: ProductRepository;
+
   let deleteProductService: DeleteProductService;
 
   beforeEach(() => {
-    deleteProductService = new DeleteProductService();
+    productRepository = new ProductRepository();
+    deleteProductService = new DeleteProductService(productRepository);
   });
 
   it('should returns error if fields are missing', async () => {
@@ -16,5 +20,18 @@ describe('Delete Product', () => {
     await expect(
       deleteProductService.execute(requestData as any)
     ).rejects.toThrow('Missing param: id');
+  });
+
+  it("should returns error if product doesn't exists", async () => {
+    const requestData = {
+      id: '1',
+      ownerId: 'teste',
+    };
+
+    jest.spyOn(productRepository, 'findById').mockResolvedValueOnce(null);
+
+    await expect(
+      deleteProductService.execute(requestData as any)
+    ).rejects.toThrow('Product not found');
   });
 });
