2d36f1aea3bf29782fc0a024d6bf83eb4d73c5f1
Rafael Mariano da Silva <seu_email@exemplo.com>
2024-10-13 11:28:05 -0300

    test: should returns error if category doesnt exists

--- src/categories/services/update-category.service.test.ts
+++ src/categories/services/update-category.service.test.ts
@@ -1,10 +1,13 @@
+import { CategoryRepository } from '../../repositories/category.repository';
 import { UpdateCategoryService } from './update-category.service';
 
 describe('Update Product', () => {
   let updateCategoryService: UpdateCategoryService;
+  let categoryRepository: CategoryRepository;
 
   beforeEach(() => {
-    updateCategoryService = new UpdateCategoryService();
+    categoryRepository = new CategoryRepository();
+    updateCategoryService = new UpdateCategoryService(categoryRepository);
   });
 
   it('Should returns error if required fields are missing', async () => {
@@ -18,5 +21,20 @@ describe('Update Product', () => {
     await expect(
       updateCategoryService.execute(requestData as any)
     ).rejects.toThrow('Missing param: id');
+  });
+
+  it("should returns error if category doesn't exists", async () => {
+    const requestData = {
+      id: '1',
+      title: 'Teste',
+      description: 'Teste',
+      ownerId: 'teste',
+    };
+
+    jest.spyOn(categoryRepository, 'findById').mockResolvedValueOnce(null);
+
+    await expect(
+      updateCategoryService.execute(requestData as any)
+    ).rejects.toThrow('Category not found');
   });
 });
