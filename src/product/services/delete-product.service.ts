2b84bd01e047cf1434644cb6ce472975c828b64b
Rafael Mariano da Silva <seu_email@exemplo.com>
2024-10-13 11:44:38 -0300

    feat: should returns error if produuct doesnt exists

--- src/product/services/delete-product.service.ts
+++ src/product/services/delete-product.service.ts
@@ -1,7 +1,8 @@
 import { MissingParamError } from '../../errors/throw-missing-param.error';
+import { ProductRepository } from '../../repositories/product.repository';
 
 export class DeleteProductService {
-  constructor() {}
+  constructor(private readonly productRepository: ProductRepository) {}
 
   async execute(deleteProductDto: any): Promise<void> {
     const requiredFields = ['id', 'ownerId'];
@@ -10,6 +11,15 @@ export class DeleteProductService {
       if (deleteProductDto[field] === undefined) {
         throw new MissingParamError(field);
       }
+    }
+
+    const product = await this.productRepository.findById(
+      deleteProductDto.id,
+      deleteProductDto.ownerId
+    );
+
+    if (!product) {
+      throw new MissingParamError('Product not found');
     }
   }
 }
