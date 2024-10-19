6074068cc98a10c457d2ba9f7c61c05a3423c37d
Rafael Mariano da Silva <seu_email@exemplo.com>
2024-10-19 10:27:29 -0300

    feat: should return all data

---
+++ src/product/services/find-all-product.service.ts
@@ -0,0 +1,10 @@
+import { ProductRepository } from '../../repositories/product.repository';
+import { Product } from '../types/product.type';
+
+export class FindAllProductService {
+  constructor(private readonly productRepository: ProductRepository) {}
+
+  execute(): Promise<Product[]> {
+    return this.productRepository.findAll();
+  }
+}
